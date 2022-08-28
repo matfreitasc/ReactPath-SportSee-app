f77a2e700076d0293de75a743a4ddd0e82696d3e
Matheus Cristofoli <matfrietasc@gmail.com>
2022-08-28 13:25:31 -0400

    Removed temporary data and switch to props

--- client/src/components/AverageSpeed/AverageSessions.jsx
+++ client/src/components/AverageSpeed/AverageSessions.jsx
@@ -8,37 +8,8 @@ import {
   Rectangle,
 } from 'recharts'
 
-function AverageSessions() {
-  const data = [
-    {
-      day: 1,
-      sessionLength: 30,
-    },
-    {
-      day: 2,
-      sessionLength: 23,
-    },
-    {
-      day: 3,
-      sessionLength: 45,
-    },
-    {
-      day: 4,
-      sessionLength: 50,
-    },
-    {
-      day: 5,
-      sessionLength: 0,
-    },
-    {
-      day: 6,
-      sessionLength: 0,
-    },
-    {
-      day: 7,
-      sessionLength: 60,
-    },
-  ]
+function AverageSessions(props) {
+  const data = props.averageSession
 
   const weekday = new Array(7)
   weekday[1] = 'M'
