5b378156df2d87bd992e3ddd69ae807cd1f0dc9c
Matheus Cristofoli <matfrietasc@gmail.com>
2022-08-29 01:46:24 -0400

    Changed the api to cloud hosted

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3000/user/'
+const baseUrl = 'https://sportsee.azurewebsites.net/user/'
 
 export default axios.create({
   baseURL: baseUrl,
