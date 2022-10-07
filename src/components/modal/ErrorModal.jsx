57511331a59344f1f4827b0bdb1ef57c83df058b
Matheus Cristofoli <matfrietasc@gmail.com>
2022-10-07 12:15:45 -0400

    fix the typo on Refresh

--- src/components/modal/ErrorModal.jsx
+++ src/components/modal/ErrorModal.jsx
@@ -47,7 +47,7 @@ export default function ErrorModal() {
                     className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
                     onClick={() => setOpen(true)}
                   >
-                    Regresh
+                    Refresh
                   </button>
                 </div>
               </Dialog.Panel>
